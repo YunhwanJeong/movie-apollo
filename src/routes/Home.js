9e114293359e07b78dd870cfa54d8ae9be5ce773
aiden-jung <jimmy53120488@gmail.com>
2020-08-12 15:09:10 +0900

    GQL: get movies 쿼리 완성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_MOVIES = gql`
+    {
+        movies {
+            id,
+            poster
+        }
+    }
+`;
 
 function Home() {
+    const result = useQuery(GET_MOVIES);
     return (
         <h1>Home</h1>
     )
