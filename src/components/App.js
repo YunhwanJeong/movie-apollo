904319d50715406a0bb0bb128d20fc9b5765f195
aiden-jung <jimmy53120488@gmail.com>
2020-08-12 15:08:57 +0900

    Setup apollo client, provider, InMemoryCache

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
 
 import Home from '../routes/Home';
 import Detail from '../routes/Detail';
+import { client } from '../index';
 
 function App() {
   return (
-    <Router>
-      <Route path="/" exact component={Home}></Route>
-      <Route path="/:id" component={Detail}></Route>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/:id" component={Detail}></Route>
+      </Router>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+export const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
