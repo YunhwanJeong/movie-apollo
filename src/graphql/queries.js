c42b98cd2829ecff1773b695457b17d962d5ea5c
aiden-jung <jimmy53120488@gmail.com>
2020-08-13 13:42:36 +0900

    GQL: 쿼리 구조 변경 및 GET_MOVIE 쿼리 추가

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,8 +1,24 @@
 import { gql } from '@apollo/client';
 
-export const GET_MOVIES = gql`{
-    movies {
-        id,
-        poster
+export const GET_MOVIES = gql`
+    query movies {
+        movies {
+            id,
+            poster
+        }
     }
-}`
+`
+
+export const GET_MOVIE = gql`
+    query movie($id: Int!) {
+        movie(id: $id) {
+            id,
+            title,
+            year,
+            genres,
+            rating,
+            poster,
+            description
+        }
+    }
+`
