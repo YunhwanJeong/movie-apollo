c21c9ae145ef3a35ce511b658b48ac1117574124
aiden-jung <jimmy53120488@gmail.com>
2020-08-13 13:43:45 +0900

    FEATURE: Movie 컴포넌트 추가

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Container = styled.div``;
-const Poster = styled.img``;
+const Container = styled.div`
+    width: 200px;
+    height: auto;
+`;
+const Poster = styled.img`
+    display: block;
+    width: 100%;
+    height: auto;
+    border-radius: 5px;
+`;
 
-function Movie (props) {
-    console.log(props);
+function Movie ({ id, poster }) {
     return (
-        <Container>
-            <Poster />
-        </Container>
+        <Link to={`/${id}`}>
+            <Container id={id}>
+                <Poster src={poster}/>
+            </Container>
+        </Link>
     )
 }
 
