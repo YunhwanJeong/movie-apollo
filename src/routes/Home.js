5ff43c4bdc01aeee1e65abb2bcb096f2be3457e5
aiden-jung <jimmy53120488@gmail.com>
2020-08-13 13:43:22 +0900

    FEATURE: Home 컴포넌트 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,28 +3,68 @@ import { useQuery } from '@apollo/client';
 import styled from 'styled-components';
 
 import Movie from '../components/Movie';
+import Loading from '../components/Loading';
 
 import { GET_MOVIES } from '../graphql/queries';
 
-const StyledHero = styled.section``;
-const StyledHero__Heading = styled.h1``;
-const StyledHero__Subheading = styled.p``;
+const StyledHero = styled.section`
+    background: linear-gradient(to right, #bc4e9c, #f80759);
+    min-height: 40vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+const StyledHero__Wrapper = styled.div`
+    color: #E2E4E4;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+const StyledHero__Heading = styled.h1`
+    font-size: 5rem;
+    margin-bottom: 20px;
+`;
+const StyledHero__Subheading = styled.p`
+    font-size: 2.5rem;
+`;
 
-const StyledMovies = styled.section``;
-const StyledMovies__Wrapper = styled.div``;
+const StyledMovies = styled.section`
+    background-color: #E2E4E4;
+    min-height: 60vh;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+`;
+const StyledMovies__Wrapper = styled.div`
+    margin-top: -50px;
+    padding-bottom: 50px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 60px;
+`;
 
-function Home() {
+function Home(props) {
+    const {
+        match: {
+            path
+        }
+    } = props;
     const { loading, data } = useQuery(GET_MOVIES);
     return (
         <>
             <StyledHero>
-                <StyledHero__Heading></StyledHero__Heading>
-                <StyledHero__Subheading></StyledHero__Subheading>
+                <StyledHero__Wrapper>
+                    <StyledHero__Heading>Apollo Movies</StyledHero__Heading>
+                    <StyledHero__Subheading>Powered by GrapqhQL</StyledHero__Subheading>
+                </StyledHero__Wrapper>
             </StyledHero>
             <StyledMovies>
-                <StyledMovies__Wrapper>
-                    { loading ? "Loading..." : data.movies.map(movie => <Movie {...movie}></Movie>)}
-                </StyledMovies__Wrapper>
+                { loading 
+                ? <Loading path={path} /> 
+                : <StyledMovies__Wrapper>
+                    {data.movies.map(movie => <Movie {...movie} key={movie.id}/>)}
+                </StyledMovies__Wrapper>}
             </StyledMovies>
         </>
     )
